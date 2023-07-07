b5ac27cc0ffd00c45fcb1e05a57acff7cd4a9fbd
Eighteen9 <w1348455938@163.com>
2023-07-07 16:08:51 +0800

    对Epoll模型实现的server和client做了一些修改

--- Socket_Server_Client/Epoll/client.c
+++ Socket_Server_Client/Epoll/client.c
@@ -4,17 +4,20 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
+#include <sys/epoll.h>
 #define MAX_BUFFER_SIZE 1024
+#define MAX_EVENTS 10
 
-int main() {
+int main()
+{
     int server_fd;
     struct sockaddr_in server_addr;
     char buffer[MAX_BUFFER_SIZE];
 
     // 创建socket文件描述符
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         perror("Failed to create socket");
         exit(EXIT_FAILURE);
     }
@@ -22,44 +25,79 @@ int main() {
     // 设置服务器地址
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8000);
-    if (inet_pton(AF_INET, "127.0.0.1", &(server_addr.sin_addr)) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &(server_addr.sin_addr)) <= 0)
+    {
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
 
     // 连接到服务器
-    if (connect(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if (connect(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("Failed to connect to server");
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected to the server.\n");
+    // 将server_fd添加进内核事件表
+    int epoll_fd = epoll_create1(0);
+    struct epoll_event event, events[MAX_EVENTS];
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event);
+   
+   //将stdin添加进内核事件表
+    event.events = EPOLLIN;
+    event.data.fd = fileno(stdin);
+    epoll_ctl(epoll_fd,EPOLL_CTL_ADD,fileno(stdin),&event);
 
-    while (1) {
-        // 从标准输入读入用户输入的消息
-        printf("Enter a message: ");
-        fgets(buffer, sizeof(buffer), stdin);
+    printf("Connected to the server.\n\n");
+    
+    while (1)
+    {
+      
+        int eventCount = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+      
+        for (int i = 0;i < eventCount; i++)
+        {
+            if (events[i].data.fd == server_fd)
+            {
+                // 接收服务器回复的消息
+                ssize_t bytes_received = recv(server_fd, buffer, sizeof(buffer), 0);
+                if (bytes_received == -1)
+                {
+                    perror("Failed to receive data");
+                    exit(EXIT_FAILURE);
+                }
 
-        // 发送消息给服务器
-        ssize_t bytes_sent = send(server_fd, buffer, strlen(buffer), 0);
-        if (bytes_sent == -1) {
-            perror("Failed to send data");
-            exit(EXIT_FAILURE);
-        }
-
-        // 接收服务器回复的消息
-        ssize_t bytes_received = recv(server_fd, buffer, sizeof(buffer), 0);
-        if (bytes_received == -1) {
-            perror("Failed to receive data");
-            exit(EXIT_FAILURE);
-        }
-
-        if (bytes_received == 0) {
-            printf("Server disconnected\n");
-            break;
-        } else {
-            buffer[bytes_received] = '\0';
-            printf("Received response from server: %s", buffer);
+                if (bytes_received == 0)
+                {
+                    printf("Server disconnected , please retry connect\n");
+                    exit(EXIT_FAILURE);
+                }
+                else
+                {
+                    buffer[bytes_received] = '\0';
+                    printf("Received data from server: %s\n\n", buffer);
+                }
+            }
+            if (events[i].data.fd == fileno(stdin))
+            {
+                // 从标准输入读入用户输入的消息
+                fgets(buffer, sizeof(buffer), stdin);
+                // 发送消息给服务器
+                ssize_t bytes_sent = send(server_fd, buffer, strlen(buffer), 0);
+                if (bytes_sent == -1)
+                {
+                    perror("Failed to send data");
+                    exit(EXIT_FAILURE);
+                }
+                printf("Send success!!!\n\n");
+                if(strncmp(buffer,"quit",4)==0)
+                {
+                    printf("Client disconnected with server successfully\n");
+                    return 0;
+                }
+            }
         }
     }
 
@@ -67,3 +105,16 @@ int main() {
 
     return 0;
 }
+
+
+
+
+/**
+ * 当客户端的socket连接意外断开时，服务端会收到一个SIGPIPE信号。这个信号通常是由客户端主动关闭socket连接引起的，但也有可能是由于网络问题或其他原因导致的。
+
+在Linux系统中，可以使用signalfd函数来注册SIGPIPE信号，并将其传递给应用程序。然后，应用程序可以使用epoll_wait函数来监听SIGPIPE信号的到来。
+一旦收到SIGPIPE信号，应用程序就可以执行相应的处理逻辑，例如重新建立连接、发送错误消息等。
+
+需要注意的是，在使用epoll模型监控socket连接时，需要确保应用程序能够正确处理SIGPIPE信号，否则可能会导致程序崩溃或出现其他异常情况。
+ * 
+*/
--- Socket_Server_Client/Epoll/server.c
+++ Socket_Server_Client/Epoll/server.c
@@ -8,16 +8,18 @@
 
 #define MAX_EVENTS 10
 #define MAX_BUFFER_SIZE 1024
-
-int main() {
-    int server_fd, client_fd, epoll_fd, event_count, i;
+#define MAX_CLIENT_CONNECT 5
+int main()
+{
+    int server_fd, client_fd, epoll_fd, event_count;
     struct sockaddr_in server_addr, client_addr;
     struct epoll_event event, events[MAX_EVENTS];
     char buffer[MAX_BUFFER_SIZE];
 
     // 创建socket文件描述符
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         perror("Failed to create socket");
         exit(EXIT_FAILURE);
     }
@@ -28,80 +30,135 @@ int main() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
     // 绑定服务器地址到socket
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("Failed to bind");
         exit(EXIT_FAILURE);
     }
 
     // 开始监听连接请求
-    if (listen(server_fd, 5) == -1) {
+    if (listen(server_fd, 5) == -1)
+    {
         perror("Failed to listen");
         exit(EXIT_FAILURE);
     }
 
     // 创建epoll文件描述符
     epoll_fd = epoll_create1(0);
-    if (epoll_fd == -1) {
+    if (epoll_fd == -1)
+    {
         perror("Failed to create epoll file descriptor");
         exit(EXIT_FAILURE);
     }
 
-     // 将server_fd添加到epoll监控中
+    // 将server_fd添加到epoll监控中
     event.events = EPOLLIN;
     event.data.fd = server_fd;
-    if (    (epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1)
+    {
         perror("Failed to add server_fd to epoll");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server started. Waiting for connections...\n");
+    // 将标准输入添加到内核事件表中
+    event.data.fd = fileno(stdin);
+    event.events = EPOLLIN;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fileno(stdin), &event);
+    printf("Server started. Waiting for connections...\n\n");
 
-    while (1) {
+    int clientFdArry[MAX_CLIENT_CONNECT] = {0};
+
+    while (1)
+    {
         // 等待事件触发
-        event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        if (event_count == -1) {
-            perror("epoll_wait error"); 
+        event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1); // 动态添加活跃事件(套接字的可读或者可写)
+        if (event_count == -1)
+        {
+            perror("epoll_wait error");
             exit(EXIT_FAILURE);
         }
 
-        //events[]里面存放了活跃事件，
-        //与select类似，只能检测活跃的事件数量，具体是那个事件，仍然需要遍历进行对比!!!
-        for (i = 0; i < event_count; i++) {     
-            if (events[i].data.fd == server_fd) {//deal with server_socket
+        // events[]里面存放了活跃事件，
+        // 与select类似，只能检测活跃的事件数量，具体是那个事件，仍然需要遍历进行对比!!!
+        for (int i = 0; i < event_count; i++)
+        {
+            if (events[i].data.fd == server_fd)
+            { // deal with server_socket
                 // 有新的连接请求
                 socklen_t client_addr_len = sizeof(client_addr);
-                client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-                if (client_fd == -1) {
-                    perror("Failed to accept connection");
-                    exit(EXIT_FAILURE);
+                for (int j = 0; j < MAX_CLIENT_CONNECT; j++)
+                {
+                    if (clientFdArry[j] == 0)
+                    {
+                        clientFdArry[j] = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+                        if (clientFdArry[j] == -1)
+                        {
+                            perror("Failed to accept connection");
+                            exit(EXIT_FAILURE);
+                        }
+                        printf("Accepted new connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                        // 将新的连接添加到epoll监控中
+                        event.events = EPOLLIN;
+                        event.data.fd = clientFdArry[j];
+                        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clientFdArry[j], &event) == -1)
+                        {
+                            perror("Failed to add client_fd to epoll");
+                            exit(EXIT_FAILURE);
+                        }
+                        break;
+                    }
                 }
-                printf("Accepted new connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-                // 将新的连接添加到epoll监控中
-                event.events = EPOLLIN;
-                event.data.fd = client_fd;
-                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event) == -1) {
-                    perror("Failed to add client_fd to epoll");
-                    exit(EXIT_FAILURE);
+            }
+            else if (events[i].data.fd == fileno(stdin))
+            { // 服务端有标准输入stdin
+                char sendBuffer[MAX_BUFFER_SIZE];
+                // printf("send message to");
+                bzero(sendBuffer, sizeof(buffer));
+                fgets(sendBuffer, MAX_BUFFER_SIZE, stdin);
+                for (int n = 0; n < MAX_CLIENT_CONNECT; n++)
+                {
+                    if (clientFdArry[n] != 0)
+                    {
+                        ssize_t sendCount = send(clientFdArry[n], sendBuffer, strlen(sendBuffer), 0);
+                        if (sendCount == -1)
+                        {
+                            perror("Failed to send data");
+                            exit(EXIT_FAILURE);
+                        }
+                        printf("Send success!!!\n\n");
+                    }
                 }
-            } else {//deal with client_socket
-                // 接收客户端发送的消息
-                ssize_t bytes_read = recv(events[i].data.fd, buffer, sizeof(buffer), 0);
-                if (bytes_read == -1) {
-                    perror("Failed to receive data");
-                    exit(EXIT_FAILURE);
-                }
-
-                if (bytes_read == 0) {
-                    // 客户端断开连接
-                    printf("Client disconnected: %d\n", events[i].data.fd);
-                    close(events[i].data.fd);
-                } else {
-                    // 打印客户端发送的消息
-                    buffer[bytes_read] = '\0';
-                    printf("Received data from client %d: %s", events[i].data.fd, buffer);
-                    // 回复消息给客户端
-                    send(events[i].data.fd, buffer, bytes_read, 0);
+            }
+            else
+            { // 若clientFd处于活跃，接收消息处理，需要遍历
+                for (int k = 0; k < MAX_CLIENT_CONNECT; k++)
+                {
+                    if (events[i].data.fd == clientFdArry[k])
+                    {
+                        ssize_t readCount = recv(events[i].data.fd, buffer, sizeof(buffer), 0);
+                        if (readCount == -1)
+                        {
+                            perror("Failed to receive data from client\n");
+                            exit(EXIT_FAILURE);
+                        }
+                        else if (readCount == 0)
+                        {
+                            printf("Client%d disconnected\n", events[i].data.fd);
+                            close(events[i].data.fd);
+                        }
+                        else
+                        {
+                            buffer[readCount] = '\0';
+                            printf("Received data from client%d: %s\n\n", events[i].data.fd, buffer);
+                            // 如果client发送了quit，则在服务端除名
+                            if (strncmp(buffer, "quit",4) == 0)
+                            {
+                                printf("client[%d] disconnected\n", events[i].data.fd);
+                                close(clientFdArry[k]);
+                                clientFdArry[k] = 0; // 客户端除名
+                            }
+                        } 
+                    }
                 }
             }
         }
